c2bc7b1327be68ff106ce38017fc2470ca6992ca
Shweta2608-code <54200526+Shweta2608-code@users.noreply.github.com>
2019-08-18 13:32:41 +0530

    Add files via upload
    First Practice program

---
+++ Demo.java
@@ -0,0 +1,20 @@
+package gettingStarted;
+
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a,b,c;
+		a= 4;
+		b=6;
+		c= a+b;
+		
+		System.out.println("Sum of a and b is"+ c);
+
+	}
+
+}
